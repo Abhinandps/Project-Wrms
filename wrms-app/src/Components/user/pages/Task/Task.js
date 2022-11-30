4e2869bc8dfb2f52059a5170e30ac7935b7db2eb
ananthu <harilalananthu@gmail.com>
2022-11-30 10:33:33 +0530

    Tasklist display completed not styled

--- wrms-app/src/Components/user/pages/Task/Task.js
+++ wrms-app/src/Components/user/pages/Task/Task.js
@@ -1,9 +1,34 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Adpop from './Adpop'
 import Main from './Main';
+import  Axios  from 'axios';
+import swal from 'sweetalert'
 
 const Task = () => {
   const [pop,setPop]=useState(false);
+  const[displays,setDisplays]=useState([]);
+
+  useEffect(()=>{
+    Axios.get('http://localhost:7000/api/list/task').then((response)=>{
+      // response.send(response.data);
+      setDisplays(response.data)
+  
+    })
+  },[])
+
+
+  const deleteitems=(Taskname)=>{
+    Axios.delete(`http://localhost:7000/api/deletelist/${Taskname}`)
+
+    swal({
+      title: "DELETED SUCCESSFULLY",
+      text: "PLEASE REFRESH THE PAGE!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+
+ }
   return(
     <>
     <div className='add'>
@@ -12,6 +37,36 @@ const Task = () => {
    <Main/>
  </Adpop>
  </div>
+
+ <div className="all-reports">
+
+{
+  displays.map((obj)=>{
+    return(
+      <div onClick={
+        () => {
+        
+          
+          }} className="report-data">
+      <div className="data">
+      <p>{obj.Taskname}</p>
+       <p>{obj.category}</p>
+       <p>{obj.startdate}</p> 
+       <p>{obj.enddate}</p> 
+       <p>{obj.priority}</p> 
+       <p>{obj.status}</p> 
+       <p><button  onClick={()=>{deleteitems(obj.Taskname)}}>Delete</button></p> 
+      <p><button>update</button></p>
+      <p><input type="number" placeholder="update status.."  required/></p>
+  </div>
+       
+     </div>
+    )
+  })
+}
+
+
+</div>
     </>
   )
 }
